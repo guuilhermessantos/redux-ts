963f0784dc753bc3d4667755f5103ed6d42bd633
Guilherme Santos <guuilhermessantos@gmail.com>
2022-08-23 09:37:02 -0300

    finalizando configuração do store

--- src/components/RepositoryList/index.tsx
+++ src/components/RepositoryList/index.tsx
@@ -1,8 +1,26 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { bindActionCreators, Dispatch } from 'redux'
 
+import { Repository } from '../../store/ducks/repositories/types';
+import * as RepositoriesActions from '../../store/ducks/repositories/actions';
+import { ApplicationState } from '../../store'
 
+interface StateProps {
+  repositories: Repository[]
+}
 
-export default function RepositoryList () {
+interface DispatchProps {
+
+}
+
+interface OwnProps {
+
+}
+type Props = StateProps & DispatchProps & OwnProps
+
+function RepositoryList (props: Props) {
+  const { repositories } = props
   return(
     <ul>
     
@@ -10,3 +28,11 @@ export default function RepositoryList () {
 
   ) 
 }
+const mapStateToProps = (state: ApplicationState) => ({
+  repositories: state.repositories.data
+})
+
+const mapDispatchToProps = (dispatch: Dispatch) => 
+  bindActionCreators(RepositoriesActions, dispatch)
+
+export default connect(mapStateToProps)(RepositoryList);
---
+++ src/store/ducks/rootReducer.ts
@@ -0,0 +1,8 @@
+// combine reducer
+import { combineReducers } from 'redux';
+
+import repositories from './repositories'
+
+export default combineReducers({
+  repositories
+})
